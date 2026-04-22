267cbd311d795d7799490e3f6777ce63cf2cc13e
IgnacioVivas <ignacab@hotmail.com>
2026-04-22 18:03:27 -0300

    actualizando rama

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
 import dotenv from 'dotenv'
 
 dotenv.config()
+dotenv.config({ path: '.env.local' })
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL ?? '',
   },
 })
